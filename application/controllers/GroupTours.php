780a2560a6e9a495aa611b1c080d638dc143c33d
software7 <software7@mid2000.com>
2019-06-25 11:01:11 +0530

    Group Tours

---
+++ application/controllers/GroupTours.php
@@ -0,0 +1,106 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class GroupTours extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function group_africa()
+	{
+		$sector ='Africa';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+	public function group_america()
+	{
+		$sector ='America';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+
+	public function group_dubai()
+	{
+		$sector ='Dubai';
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+
+
+	public function group_europe()
+	{
+		$sector ='Europe';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+
+	public function group_japan()
+	{
+		$sector ='Japan';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+
+	public function group_mauritius()
+	{
+		$sector ='Mauritius';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+	
+
+public function group_australiaandnewzealand()
+	{
+		$sector ='Australia and New Zealand';
+
+		$data['api_result']=$this->call_api($sector);
+		$this->load->view('common/header');
+		$this->load->view('group_tours/view',$data);
+		$this->load->view('common/footer');
+	}
+
+	public function call_api($sector){
+		$api_url ='http://203.112.144.254:8888/WebsiteData/WebsiteDataService.svc/getProductListBySectorForWebsite?SectorName='.$sector;
+
+		$ch = curl_init($api_url);
+		$username = "mhwebsite";
+		$password = "mango";
+		$headers = array(
+		    'Content-Type:application/json',
+		    'UserName:'.$username,
+		    'Password:'.$password
+		);
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+		curl_setopt($ch, CURLOPT_TIMEOUT, 86400);
+		curl_setopt($ch, CURLOPT_LOW_SPEED_LIMIT, 10240);
+		curl_setopt($ch, CURLOPT_LOW_SPEED_TIME, 60);
+		$return = curl_exec($ch);
+		curl_close($ch);
+		$decrypt_data = json_decode($return);
+		return $decrypt_data->ProductList;
+
+	}
+
+}
--- application/views/tours/tour_details.php
+++ application/views/tours/tour_details.php
@@ -1,4 +1,5 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
 <script>
 var base64Img = null;
 imgToBase64('../assets/images/Mango-Holidays.png', function(base64) {
@@ -28,11 +29,12 @@ generate = function()
         }, 
         margins);
         
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
+    // var iframe = document.createElement('iframe');
+    // iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
     // document.body.appendChild(iframe);
     
-    iframe.src = pdf.output('save', 'filename.pdf');
+    // iframe.src = 
+    pdf.output('save', 'filename.pdf');
 };
 
 function headerFooterFormatting(doc, totalPages)
@@ -97,39 +99,51 @@ function footer(doc, pageNumber, totalPages){
 
  </script>
 
-  <script>
 
- var doc = new jsPDF(); 
-var specialElementHandlers = { 
-    '#editor': function (element, renderer) { 
-        return true; 
-    } 
-};
-$('#submit').click(function () { 
-    doc.fromHTML($('#html-2-pdfwrapper').html(), 15, 15, { 
-        'width': 190, 
-            'elementHandlers': specialElementHandlers 
-    }); 
-    doc.save('sample-page.pdf'); 
-});
+ <script type="text/javascript">
+     
+     $(document).ready(function(){
 
-</script>
+    //pdf 다운로드  
+    $("#pdfDownloader").click(function(){
+    
+        html2canvas(document.getElementById("PDFcontent"), {
+            onrendered: function(canvas) {
 
-
-  <?php 
-        
-            if(isset($itenary)){
-                $itenary_program='<div style="border: 1px solid #ccc;margin: 2%;padding: 2%;">';
-                foreach($itenary as $itenary_key){
-
-                        $itenary_program .="<h3 style='font-size:20px'> Day  ".$itenary_key->DayNo." : ".$itenary_key->DayTitle."</h3>";
-                        $itenary_program .="<p>".mb_convert_encoding($itenary_key->DayProgram,'UTF-8')."</p>";
-            }}
-                $itenary_program .='</div>';
+                var imgData = canvas.toDataURL('image/png');
+                var doc = new jsPDF('p', 'mm', [500, 500]); //210mm wide and 297mm high
                 
-        ?>
-<div id='PDFcontent'>
+                doc.addImage(imgData, 'PNG', 10, 10);
+                doc.save('sample.pdf');
+            }
+        });
 
+    });
+    
+    
+})
+ </script>
+<?php 
+        
+    if(isset($itenary)){
+        $itenary_program='<div style="border: 1px solid #ccc;margin: 2%;padding: 2%;">';
+        foreach($itenary as $itenary_key){
+
+                $itenary_program .="<h3 style='font-size:20px'> Day  ".$itenary_key->DayNo." : ".$itenary_key->DayTitle."</h3>";
+                $itenary_program .="<p>".mb_convert_encoding($itenary_key->DayProgram,'UTF-8')."</p>";
+    }}
+        $itenary_program .='</div>';
+                
+?>
+<div id="tour_heading">
+    <?php echo $ProductTitle;?>
+</div>
+<div id="tour_duration">
+     Days <?php  echo $Days; ?> / Night <?php echo $Nights; ?>
+</div>
+<div id='PDFcontent'>
+    <p align="center" style="font-weight: bold"><?php echo $ProductTitle;?></p>
+    <p align="center"> Days <?php  echo $Days; ?> / Night <?php echo $Nights; ?></p>
     <p><?php echo str_replace('"'," ",$itenary_program);?></p>
 
 </div>
@@ -140,7 +154,7 @@ $('#submit').click(function () {
 
 </div>
 
-<button id='gpdf'>Generate PDF</button>
+<button id='pdfDownloader'>Generate PDF</button>
 <script type="text/javascript">
     
     var pdfdoc = new jsPDF();
