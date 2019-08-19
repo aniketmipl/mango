36533991f46872e46665887d6d58cdf3b3c5a54c
web11 <web11@mid2000.com>
2019-08-19 13:59:12 +0530

    pulling

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -47,7 +47,7 @@
                                 <li><a href="<?php echo base_url();?>GroupTours/group_europe"> Europe</a></li>
                                 <li><a href="<?php echo base_url();?>GroupTours/group_japan"> Japan</a></li>
                                 <!-- <li><a href="<?php echo base_url();?>GroupTours/group_mauritius"> Mauritius</a></li> -->
-                                <li><a href="<?php echo base_url();?>GroupTours/group_rest_of_europe"> Rest of Europe</a></li>
+                                <!-- <li><a href="<?php echo base_url();?>GroupTours/group_rest_of_europe"> Rest of Europe</a></li> -->
                             </ul>
                             </div>
                         </div>
