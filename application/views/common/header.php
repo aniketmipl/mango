5a3e4cd0ce848b10504e5e1943f9cafcde797419
software7 <software7@mid2000.com>
2019-09-11 17:15:08 +0530

    header

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -27,6 +27,9 @@
     if($controller=='customized-holidays'){?>
         <title>Customized <?php echo $sector ?> Tour Packages from Mumbai,Pune,India| Mango Holidays</title>
     <?php } 
+    else if($controller == 'group-tour-packages'){ ?>
+        <title><?php echo $sector ?> Group Tour Packages from Mumbai,Pune,India| Mango Holidays</title>
+    <?php }
     if(isset($title)){?>
         <title><?php echo @$title;?></title>
     <?php } else { ?>
