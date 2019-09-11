3d6da427d79a00696201b8404cb841cfa296ff29
web11 <web11@mid2000.com>
2019-09-11 17:29:15 +0530

    pulling

--- application/views/pages/guest_testimonials_all.php
+++ application/views/pages/guest_testimonials_all.php
@@ -26,120 +26,113 @@
         </div>
     </div>
 </div> -->
+
 <!-- Strat Blog Section -->
-<section class="services-style-one testimonials">
-<div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>WANKHADE & MODANI FAMILY PUNE & MRS. LEELA SHIURKAR, NASIK</h4>
-                            <p>Our Exotic Europe tour with Mango Holidays was a treasure of beautiful memories. Till date we have travelled with many tour operators, but Mango Holidays’s Europe tour is by far the finest example of the best arrangements for sightseeing, food and premium hotels!</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+<section class="blog-area guest">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
+                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR & MRS. TAMBEKAR, AHMEDABAD</h4>
-                            <p>We are very pleased with the travel arrangements made for us through Mango Holidays. I just cant express how smoothly our dream holiday went without any hiccups. We are truly overjoyed with our experience. This will be a treasured memory for as long as we live. We wish Mango Holidays all the very best for future!</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
+                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MRS. SHIRKE, PUNE & MRS. VASUNDHARA RAJE BHOSLE, NAGPUR</h4>
-                            <p>Thank you very much Mango Holidays for excellent management on our Exotic Europe tour. It was a very well designed tour itinerary, premium hotels and a variety of Indian & local food! We had a truly memorable experience and got much more than we expected. Mango Holidays means 100% satisfaction.</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
-                        </div>
-                    </div>  
+                    </div>
                 </div>
-                </div> 
-                <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR. VIJAY DESHPANDE & FAMILY, PUNE</h4>
-                            <p>We travelled with Mango Holidays to Europe in 2011 & we had such a great experience that we travelled to USA also in 2012. We had a fabulous tour. We look forward to many more tours with you!</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
+                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR. & MRS. VILAS BONGALE, MUMBAI</h4>
-                            <p>We had a fabulous time on our tour. The arrangements, sightseeing and hotels were excellent. We wish Mango Holidays all the very best!!</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
+                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR. LUNKAD & FAMILY, PUNE</h4>
-                            <p>Your vast knowledge has really helped us to extend our information. We wish that we can travel to all destinations with Mango Holidays and we hope that you will show our grandfather all of Europe too.</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
-                        </div>
-                    </div>  
+                    </div>
                 </div>
-                </div> 
-                <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>T. N. AROLKAR</h4>
-                            <p>We would like to thank you very much for the fun and memorable Glimpses of Europe trip from 04th June – 15th June 2018!
-I would like to thank you very much for all the efforts you have done, for the wonderful, streamlined tour we recently experienced with your organization. We really appreciate your support regarding the organization of our trip.</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
+                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR. SHIVAJI PAWAR & FRIENDS, MUMBAI</h4>
-                            <p>This is to place on record, our sincere thanks to you for guiding us on our Europe Tour organised by Mango Holidays. We have in the past, undertaken several tours but without any inhibition, we can say that this Europe tour was the best. Your tour manager was truly versatile. Your abillity of giving information about particular places of interest was flawless. Looking forward to more such golden moments with MANGO Holidays.</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
+                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
                         </div>
-                    </div>  
-                </div>  
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MR. VILAS RATHOD, DIWAN SURYAVANSHI, DHULE, RANJIT DESHMUKH & MANE, ICHALKARANJI</h4>
-                            <p>Our Europe Tour was truly a dream come true. Mango Holidays put in every effort to make our tour a cherished experience. We tell all our relatives and friends that if they are travelling to Europe, they must travel ONLY with Mango Holidays.</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
-                        </div>
-                    </div>  
+                    </div>
                 </div>
-                </div> 
-                <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 column-margin">
-                    <div class="services-features">
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <div class="back-color">
-                            <h4>MRS. PRATIMA JOSHI & GROUP, PUNE</h4>
-                            <p>Our experience with Mango Holidays is unforgettable. We were given the best arrangements in all aspects but we particularly wish to mention here that the best part was that Mr. Milind Babar, who has an experience of 17 years, accompanied us on the tour. He did not just show us Europe but he explained to us, everything right from the history, geography, culture and lifestyle of Europe!</p>
-                             <!-- <a class="btn btn-default custom-btn-four my-btn-sm" href="#" role="button">Read More</a> -->
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
+                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
                         </div>
-                    </div>  
-                </div> 
-                </div> 
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
+                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+           
+           
+            
+            
+       </div>
+       <div class="row">
+            <div class="col-sm-12">
+                <div class="nex-tab-btn text-center">
+                    <ul class="list-inline">
+                        <li><a href="#"><i class="ion-arrow-left-c"></i></a></li>
+                        <li><a class="active" href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#"><i class="ion-arrow-right-c"></i></a></li>
+                    </ul>
+                </div>
             </div>
         </div>
+    </div>
 </section>
 <!-- End Blog Section -->
 
