1a405f5852dc8d9f94890fee1c97d18e53b60596
web11 <web11@mid2000.com>
2019-09-12 10:59:18 +0530

    pulling

--- application/views/pages/guest_testimonials_all.php
+++ application/views/pages/guest_testimonials_all.php
@@ -34,7 +34,7 @@
         <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title=" Mr.Ravindra Deomore">
                         <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
@@ -54,8 +54,8 @@
             <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
-                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <a href="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Hendre.jpg" class="lightbox-image" title="Mr. & Mrs. Hendre">
+                        <img src="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Hendre.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
                                 <i class="fa fa-search-plus"></i>
@@ -65,9 +65,9 @@
                     </div>
                     <div class="tour-item blog-item">
                         <div class="blog-details">
-                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr. & Mrs. Hendre</h4>
+                            <p>We Selected Mango Holidays as our travel partner after a lot of research and I am happy to say that it was a perfect choice. Mango Holidays brought our Europe dream into reality and we are extremely thankful to them for the same. We will definitely recommend Mango Holidays to everyone.</p>
+                            
                         </div>
                     </div>
                 </div>
@@ -75,8 +75,8 @@
             <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
-                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <a href="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Dimbar.jpg" class="lightbox-image" title="Mr. & Mrs. Dimbar">
+                        <img src="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Dimbar-S.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
                                 <i class="fa fa-search-plus"></i>
@@ -86,8 +86,8 @@
                     </div>
                     <div class="tour-item blog-item">
                         <div class="blog-details">
-                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr. & Mrs. Dimbar</h4>
+                            <p>Mango Holidays Managed to turn our tour into memories which we will cherish for the rest of our life. The Hotels, Restaurants, coach, Tour Manager and sightseeing were all top notch. The meticulous planning was clearly visible during the tours. Hats off to Mango Holidays.</p>
                         </div>
                     </div>
                 </div>
@@ -95,8 +95,8 @@
             <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
-                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <a href="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Sathe.jpg" class="lightbox-image" title="Mr. & Mrs. Sathe">
+                        <img src="<?php echo base_url();?>assets/images/guest/Mr-Mrs-Sathe.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
                                 <i class="fa fa-search-plus"></i>
@@ -106,8 +106,10 @@
                     </div>
                     <div class="tour-item blog-item">
                         <div class="blog-details">
-                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <h4 class="guest-name"><i class="ion-person"></i> Mr. & Mrs. Sathe</h4>
+                            <p>
+                            We attended the Europe Melava in Thane last year and it made a huge positive impact on us. The no discount policy, the product differentiation and individual care for the guest were a few attributes that really impressed us. We booked the tour then and there and I am glad to say that we made the right choice. Our heartfelt thanks to the entire team at Mango holidays.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -115,8 +117,8 @@
             <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
-                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <a href="<?php echo base_url();?>assets/images/guest/Chandak-Group.jpg" class="lightbox-image" title="Mrs. Chandak & Group">
+                        <img src="<?php echo base_url();?>assets/images/guest/Chandak-Group.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
                                 <i class="fa fa-search-plus"></i>
@@ -126,8 +128,10 @@
                     </div>
                     <div class="tour-item blog-item">
                         <div class="blog-details">
-                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <h4 class="guest-name"><i class="ion-person"></i> Mrs. Chandak & Group</h4>
+                            <p>
+                            We always travel together as a ladies group. Six of us joined Mango Holidays for the Exotic Europe tour this year. We are extremely happy with our tour. We enjoyed exploring various places and the new friendships we made will last a lifetime. Mango holidays is the best choice for any travel plans.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -135,8 +139,8 @@
             <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
                 <div class="hover-box tour-blog-box blog-item-1">
                     <div class="tour-img image">
-                        <a href="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" class="lightbox-image" title="Image Caption Here">
-                        <img src="<?php echo base_url();?>assets/images/guest/Ravindra-Deomore.jpg" alt="">
+                        <a href="<?php echo base_url();?>assets/images/guest/Ms-Smita-Samant.jpg" class="lightbox-image" title="Mrs. Smita Samant & Family">
+                        <img src="<?php echo base_url();?>assets/images/guest/Ms-Smita-Samant.jpg" alt="">
                         <div class="over-layer">
                             <div class="links">
                                 <i class="fa fa-search-plus"></i>
@@ -146,8 +150,54 @@
                     </div>
                     <div class="tour-item blog-item">
                         <div class="blog-details">
-                            <h4 class="guest-name"><i class="ion-person"></i> Mr.Ravindra Deomore</h4>
-                            <p>Mango Holidays Indeed delivered an exotic tour as promised. Right from the booking stage till tour execution, everything was perfectly planned and delivered. We are truly indebted to Mango Holidays for giving us an experience of a lifetime.</p>
+                            <h4 class="guest-name"><i class="ion-person"></i> Mrs. Smita Samant & Family</h4>
+                            <p>
+                            We were so happy to have found Mango Holidays during our research. The service from start to finish was impeccable.  We were able to enjoy a stress and worry-free trip with the help of  Mango Holidays and their expert planning. The service was wonderful - everyone was super helpful and informative.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Mr-Amit-Mrs-Maitreyee-Samel.jpeg" class="lightbox-image" title="Mrs. Maitreyee Samel">
+                        <img src="<?php echo base_url();?>assets/images/guest/Mr-Amit-Mrs-Maitreyee-Samel-s.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
+                        </div>
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Mrs. Maitreyee Samel</h4>
+                            <p>
+                            Everyone at Mango Holidays was very responsive, knowledgeable and friendly. Our trip was fabulous and exactly as I had hoped and pictured. Mango Holidays provided the right mix of a planned tour. Our accommodations were fabulous, restaurant recommendations were good, the itinerary info was perfect. Thanks so much!"
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12" style="display:inline-flex; float:none;">
+                <div class="hover-box tour-blog-box blog-item-1">
+                    <div class="tour-img image">
+                        <a href="<?php echo base_url();?>assets/images/guest/Dr-Gupta.jpg" class="lightbox-image" title="Dr. Gupta ">
+                        <img src="<?php echo base_url();?>assets/images/guest/Dr-Gupta.jpg" alt="">
+                        <div class="over-layer">
+                            <div class="links">
+                                <i class="fa fa-search-plus"></i>
+                            </div>
+                        </div>
+                        </a>
+                    </div>
+                    <div class="tour-item blog-item">
+                        <div class="blog-details">
+                            <h4 class="guest-name"><i class="ion-person"></i> Dr. Gupta </h4>
+                            <p>
+                            Mango Holidays made sure our holiday was hassle free. We have experienced them twice and plan to in the future as well."
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -158,7 +208,7 @@
             
             
        </div>
-       <div class="row">
+       <!-- <div class="row">
             <div class="col-sm-12">
                 <div class="nex-tab-btn text-center">
                     <ul class="list-inline">
@@ -171,7 +221,7 @@
                     </ul>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
 <!-- End Blog Section -->
