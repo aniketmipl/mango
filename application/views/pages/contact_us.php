5c7baab7849c6fa7632f9db49c8cc1280bfe6670
web11 <web11@mid2000.com>
2019-10-09 11:45:38 +0530

    pulling

--- application/views/pages/contact_us.php
+++ application/views/pages/contact_us.php
@@ -130,7 +130,8 @@ var randomnumber= <?= $mynumber?>;
 			<a href="https://www.facebook.com/MangoHolidays/" target="_blank"><i class="fa fa-facebook"></i><span></span></a>&nbsp;&nbsp;
             <a href="https://www.instagram.com/mangoholidays/" target="_blank"><i class="fa fa-instagram"></i></a>&nbsp;&nbsp;
             <a href="https://twitter.com/mangoholidays" target="_blank"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;
-            <a href="https://www.youtube.com/channel/UCUl_GfhPaCEidqnHeff0njQ" target="_blank"><i class="fa fa-youtube fa-lg"></i></a>
+			<a href="https://www.youtube.com/channel/UCUl_GfhPaCEidqnHeff0njQ" target="_blank"><i class="fa fa-youtube fa-lg"></i></a>&nbsp;&nbsp;
+			<a href="https://api.whatsapp.com/send?phone=918380039499" target="_blank"><i class="fa fa-whatsapp fa-lg"></i></a>
 			<!-- <a href="#"><i class="fa fa-facebook"></i><span></span></a> &nbsp;&nbsp;
 			<a href="#"><i class="fa fa-google-plus"></i></a> &nbsp;&nbsp;
             <a href="#"><i class="fa fa-twitter"></i></a> &nbsp;&nbsp;
