5a4a5c7c9d06fc306541b957a2fb2531e12d01dc
web11 <web11@mid2000.com>
2019-11-09 15:40:08 +0530

    pulling

--- application/controllers/Tours.php
+++ application/controllers/Tours.php
@@ -2969,10 +2969,10 @@ $this->load->view('common/header',$head_data);
 		
 		$data['api_result']=$this->call_api($product_id,$product_code);	
 		$filter_data['complete_data'] = $data['api_result']['complete_data'];
-		// $filter_data['slider_image'] = 'git/CH2/all-of-china-banner.jpg';
-		// $filter_data['slider_mobile_image'] = 'git/CH2/all-of-china.jpg';
-		$filter_data['slider_image'] = 'images/default-banner.jpg';
-		$filter_data['slider_mobile_image'] = 'images/default-tour.jpg';
+		$filter_data['slider_image'] = 'fit/CH2/all-of-china-banner.jpg';
+		$filter_data['slider_mobile_image'] = 'fit/CH2/all-of-china.jpg';
+		//$filter_data['slider_image'] = 'images/default-banner.jpg';
+		//$filter_data['slider_mobile_image'] = 'images/default-tour.jpg';
 		$filter_data['sector']="Asia";
 		$head_data['sector'] = $filter_data['sector'];
 		$head_data['travel_type'] = $filter_data['complete_data']->TravelType;
