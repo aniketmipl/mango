6f6ddc6f1713b8cc3cd06599a1ea0352624309df
software7 <software7@mid2000.com>
2020-01-11 11:59:09 +0530

    add tour

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -288,6 +288,7 @@
                                                         </div>
                                                         <div class="col-md-6 col-sm-12">
                                                             <li><a href="<?php echo base_url()?>customized-holidays/india/himachal-pradesh">Himachal</a></li>
+                                                            <li><a href="<?php echo base_url()?>customized-holidays/india/ladakh">Ladakh</a></li>
                                                             <li><a href="<?php echo base_url()?>customized-holidays/india/madhya-pradesh">Madhya Pradesh</a></li>
                                                             <li><a href="<?php echo base_url()?>customized-holidays/india/rajasthan">Rajasthan</a></li>
                                                             <li><a href="<?php echo base_url()?>customized-holidays/india/tamil-nadu">Tamil Nadu</a></li>
